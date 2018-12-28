fb4a475ddda4bd279867adc051e4781ed85b6e4c
unknown <chowchin@gmail.com>
2018-12-28 08:53:01 +0800

    Completed sendSms

--- test.js
+++ test.js
@@ -3,9 +3,16 @@ const HuaweiApi = require('.')
 
 let client
 
-test('create client and auth', async t => {
+test('create client and send sms', async t => {
   client = new HuaweiApi('192.168.8.1')
   await client.login('admin', 'password')
   await client.sendSms('91234567', 'Hello World')
+  t.end()
+})
+
+test('device info', async t => {
+  client = new HuaweiApi('192.168.8.1')
+  let deviceInfo = await client.deviceInfo()
+  console.log('deviceInfo', deviceInfo)
   t.end()
 })
